98e3d4520ad0129e48e28d914bd45d0f95a47535
Maxwell <166428177+Maxwellspring@users.noreply.github.com>
2025-05-13 10:44:01 -0700

    it looks better

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ import { LuminosityShader } from 'three/addons/shaders/LuminosityShader.js';
 const loader = new GLTFLoader();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 10000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 const controls = new OrbitControls(camera, renderer.domElement);
 
-renderer.domElement.addEventListener("mouseup", onClick, false);
+renderer.domElement.addEventListener("mousedown", onClick, false);
+
+renderer.domElement.addEventListener("mousemove", onMouseMove, false);
 
 const mouse = new THREE.Vector2();
 const raycaster = new THREE.Raycaster();
@@ -32,6 +34,98 @@ window.addEventListener('resize', () => {
     // }
 
 }, false);
+
+let intersecting = false; // Variable to store the currently intersected object
+let intersectedObject = null; // Variable to store the currently intersected object
+
+
+const cursorText = document.getElementById('cursor-text');
+
+document.addEventListener('mousemove', (event) => {
+    cursorText.style.left = event.clientX + 'px';
+    cursorText.style.top = event.clientY + 13 + 'px';
+
+});
+
+document.addEventListener('mouseout', () => {
+    cursorText.style.display = 'none';
+});
+
+function onMouseMove(event) {
+    event.preventDefault(); // Prevent default browser behavior (e.g., text selection)
+    // Get the normalized mouse coordinates
+    mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
+    mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
+
+    // Update the raycaster's position
+    raycaster.setFromCamera(mouse, camera);
+
+    // Find intersections
+    const intersects = raycaster.intersectObjects(scene.children, true);
+
+    // console.log(intersects.distance)
+
+    // Check if there are any intersections
+    if (intersects.length > 0) {
+        intersecting = true;
+        let intersectedObject = intersects[0].object; // Store the intersected object
+        const firstIntersection = intersects[0];
+        const distanceToIntersection = firstIntersection.distance;
+        console.log("Distance to intersection:", distanceToIntersection);
+        // cursorText.textContent = 'Click to interact'; // Change text on hover
+        if (intersectedObject.uuid == cubeC.uuid) {
+            console.log("HOVER!");
+            // window.open("about-me.html", "_self");
+            cursorText.textContent = "About Me";
+            // cursorText.style.fontSize = 10 * distanceToIntersection / 2 + 'px'; // Adjust font size based on distance
+            cursorText.style.display = 'block';
+        } else if (intersectedObject.uuid == cubeS.uuid) {
+            console.log("HOVER!");
+            // window.open("contact.html", "_self");
+            cursorText.textContent = "Contact";
+            // cursorText.style.fontSize = 10 * distanceToIntersection / 2 + 'px'; // Adjust font size based on distance
+
+            cursorText.style.display = 'block';
+
+        } else if (intersectedObject.uuid == cube.uuid) {
+            console.log("HOVER!");
+            // window.open("portfolio.html", "_self");
+            cursorText.textContent = "Portfolio";
+            // cursorText.style.fontSize = 10 * distanceToIntersection / 2 * -1 + 'px'; // Adjust font size based on distance
+
+            cursorText.style.display = 'block';
+
+        } else if (intersectedObject.uuid == cubeSad.uuid) {
+            console.log("HOVER!");
+            // window.open("resume.html", "_self");
+            cursorText.textContent = "Resume";
+            // cursorText.style.fontSize = 10 * distanceToIntersection / 2 + 'px'; // Adjust font size based on distance
+
+            cursorText.style.display = 'block';
+            // importedModel && importedModel.children.includes(intersectedObject.uuid)
+        } else if (intersectedObject.parent.parent.uuid == importedModel.uuid) {
+            console.log("HOVER!");
+            cursorText.textContent = "A model I created myself";
+            // cursorText.style.fontSize = 10 * distanceToIntersection / 2 + 'px'; // Adjust font size based on distance
+
+            cursorText.style.display = 'block';
+        } else {
+            console.log("NOT HOVER!")
+            // cursorText.style.fontSize = 10 * distanceToIntersection / 2 + 'px'; // Adjust font size based on distance
+
+            intersecting = false;
+            intersectedObject = null; // Reset the intersected object
+            cursorText.textContent = ''; // Clear text when not hovering over an object
+            cursorText.style.display = 'none';
+        }
+    } else {
+        intersecting = false;
+        intersectedObject = null; // Reset the intersected object
+        cursorText.textContent = ''; // Clear text when not hovering over an object
+        cursorText.style.display = 'none';
+    }
+}
+
 
 function onClick(event) {
     event.preventDefault(); // Prevent default browser behavior (e.g., text selection)
@@ -61,6 +155,8 @@ function onClick(event) {
         } else if (intersectedObject.uuid == cubeSad.uuid) {
             console.log("Clicked on the cubeSad");
             window.open("resume.html", "_self");
+        } else {
+            console.log("Clicked on something else");
         }
     }
 }
@@ -70,6 +166,8 @@ function onClick(event) {
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap; // Or THREE.PCFShadowMap ====================================================================
+
 document.body.appendChild(renderer.domElement);
 
 const texturey = new THREE.TextureLoader().load('texturours.png');
@@ -181,14 +279,21 @@ cubeS.castShadow = true; // the cube can cast shadows
 cubeS.receiveShadow = true;
 cubeC.receiveShadow = true;
 cubeC.castShadow = true;
-floor.receiveShadow = true;
-floor.castShadow = true; // the cube can cast shadows
+// floor.receiveShadow = true;
+// floor.castShadow = true; // the cube can cast shadows
 cubeSad.receiveShadow = true;
 cubeSad.castShadow = true; // the cube can cast shadows
 
-directionalLight.shadow.mapSize.width = 1024 * 10; // Default is 512
-directionalLight.shadow.mapSize.height = 1022 * 10;
+// sphere.receiveShadow = false;
+
+
+let multipliedShadowRes = 10
+
+directionalLight.shadow.mapSize.width = 1024 * multipliedShadowRes; // Default is 512
+directionalLight.shadow.mapSize.height = 1022 * multipliedShadowRes;
 ambientLight.intensity = 1;
+
+
 
 cubeS.shadowMaterial = shadowOverrideMaterial;
 
@@ -207,10 +312,12 @@ loader.load("platform.glb", (gltf) => {
     platform.position.set(0, -3, 0); // Set the position of the platform
 });
 
+let importedModel = null;
+
 loader.load("T-2.13-other-MODEL.glb", (gltf) => {
-    const platform = gltf.scene;
-    scene.add(platform);
-    platform.traverse((child) => {
+    importedModel = gltf.scene;
+    scene.add(importedModel);
+    importedModel.traverse((child) => {
         if (child.isMesh) {
             child.castShadow = true;
             child.receiveShadow = true;
@@ -218,9 +325,9 @@ loader.load("T-2.13-other-MODEL.glb", (gltf) => {
         }
     });
 
-    platform.scale.set(0.01, 0.01, 0.01); // Scale to twice its original size in all dimensions
-    platform.position.set(0, -1.72, 0); // Set the position of the platform
-    platform.rotation.set(0, -90, 0); // Set the rotation of the platform
+    importedModel.scale.set(0.01, 0.01, 0.01); // Scale to twice its original size in all dimensions
+    importedModel.position.set(0, -1.72, 0); // Set the position of the platform
+    importedModel.rotation.set(0, -90, 0); // Set the rotation of the platform
 });
 
 const pointLight = new THREE.PointLight(0xffffff, 1, 100, 1);
@@ -263,13 +370,21 @@ function Vector3(x, y, z) {
     return new THREE.Vector3(x, y, z);
 }
 
+const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, { // Adjust size for quality/performance
+    format: THREE.RGBAFormat,
+    type: THREE.FloatType // Or HalfFloatType for better performance
+});
+const cubeCamera = new THREE.CubeCamera(0.1, 1000, cubeRenderTarget);
 
 directionalLight.shadow.camera.near = 0.1;
-directionalLight.shadow.camera.far = 100;
-directionalLight.shadow.camera.left = -5;
-directionalLight.shadow.camera.right = 5;
-directionalLight.shadow.camera.top = 5;
-directionalLight.shadow.camera.bottom = -5;
+directionalLight.shadow.camera.far = 1000;
+directionalLight.shadow.camera.left = -100;
+directionalLight.shadow.camera.right = 100;
+directionalLight.shadow.camera.top = 10;
+directionalLight.shadow.camera.bottom = -100
+
+directionalLight.shadow.bias = -0.000091; // Try this value
+;
 
 
 scene.add(cube);
@@ -293,6 +408,8 @@ directionalLight.position.set(1, 3, 1); // Set the direction of the light
 ambientLight
 
 camera.position.z = 5;
+// camera.position.y = -1;
+// camera.rotation.x = -0.5;
 
 let switchty = false;
 
@@ -310,6 +427,11 @@ function animate() {
     // ambientLight.intensity += 0.01
     // directionalLight.intensity += 0.01
     // console.log(directionalLight.intensity)
+
+    // cubeS.onBeforeRender = function () {
+    //     cubeCamera.position.copy(cubeS.position);
+    //     cubeCamera.update(renderer, scene);
+    // };
 
 
     changeLightIntensity()
